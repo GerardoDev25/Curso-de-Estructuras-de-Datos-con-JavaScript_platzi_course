4d7c25e1d92c1a1419b18c1117d702df8a6a1953
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-01 23:01:54 -0400

    Agregar nodos a la lista

--- linkedList/sinly.js
+++ linkedList/sinly.js
@@ -1,22 +1,22 @@
 // 1-- > 2-- > 3-- > 4-- > 5-- > null;
 
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-};
+// let singlyLinkedList = {
+//   head: {
+//     value: 1,
+//     next: {
+//       value: 2,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 4,
+//           next: null,
+//         },
+//       },
+//     },
+//   },
+// };
 
-class Nodo {
+class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
@@ -32,5 +32,22 @@ class MySinglyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
+
 let myLinkedList = new MySinglyLinkedList(1);
+console.log(myLinkedList);
+console.table(myLinkedList.append(2));
