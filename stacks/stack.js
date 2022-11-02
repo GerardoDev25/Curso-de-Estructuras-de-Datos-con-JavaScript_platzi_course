9b74f3199ccd781799341d93a9562c5c8de98493
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-01 23:54:47 -0400

    Construyendo un Stack

---
+++ stacks/stack.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.botton = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.botton = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.botton) this.botton = null;
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+}
+
+const myStack = new Stack();
